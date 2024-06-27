74f2f50ddebcebd8e85f3ea83ba14b923117b0f5
DavidSanzRaya <151066500+DavidSanzRaya@users.noreply.github.com>
2024-06-27 03:35:25 +0200

    menu back to the moon

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Knownt
+{
+    public class ScenesManager : MonoBehaviour
+    {
+        public void LoadScene(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        public void LoadGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
